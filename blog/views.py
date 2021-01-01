c733f78f6761656107f9543b1ba0cbf74f2685be
William Rodrigues <williamrodrigues2442@gmail.com>
2020-12-31 22:34:08 -0300

    added data from database to template

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,14 @@
+from django.utils import timezone
 from django.shortcuts import render
+
+from .models import Post
 
 
 def post_list(request):
     """Main page, display all posts."""
-    context = {}
+    posts = Post.objects.filter(
+        published_at__lte=timezone.now()
+    ).order_by('-published_at')
+
+    context = {'posts': posts}
     return render(request, 'blog/post_list.html', context)
